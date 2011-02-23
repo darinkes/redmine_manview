01a191f47f7345dce14c8ce149aca3722f9447e8
Stefan Rinkes <stefan.rinkes@gmail.com>
2011-02-23 08:56:32 +0100

    improve manpage-parser
    sort some Regexen
    
    Parse some new man-tags

--- app/helpers/manview_helper.rb
+++ app/helpers/manview_helper.rb
@@ -4,16 +4,20 @@ module ManviewHelper
 
     atext = text.split("\n")
     ntext = Array.new
+    close_tr = false
+    columns = nil
 
-    atext.each do | line |
+    atext.each_index do | index |
+      line = atext[index].to_s
+
       # stuff we don't want
       line.sub!(/^\s*/, '')
       line.sub!(/^\.\\\".*/, '')
       line.sub!(/^\.Dt.*/, '')
       line.sub!(/^\.Dd.*/, '')
       line.sub!(/^\.Os/, '')
-      line.sub!(/^\.Nd/, ' - ')
-      line.sub!(/^\.Pp/, '<br><br>')
+      line.sub!(/^\.Dv\s*/, '')
+      line.sub!(/^\.Li\s*/, '')
       line.sub!(/\\&$/, '')
 
       # escape line to be able to show e.g. include-paths
@@ -28,33 +32,109 @@ module ManviewHelper
       line.sub!(/^\.At v([0-9\.]+).*/, "<em>Version \\1 AT&T UNIX.</em>")
 
       # serious stuff
-      # order matters here!
-      line.sub!(/^\.Nm\s+(.+)\s*(,)*/, " <b>\\1</b>\\2 ")
-      line.sub!(/^\.Xr\s*([a-zA-Z\.]+)\s*([0-9]+)\s*(,)*/, "<a href=\"search?manview[man_category]=\\2&manview[man_name]=\\1&manview[strict]=true\">\\1(\\2)</a>\\3")
+      line.sub!(/^\.Nd/, ' - ')
+      line.sub!(/^\.Pp/, '<br><br>')
+      line.sub!(/^\.Xr\s+(.+)\s+([0-9]+)\s*(,)*/, " <a href=\"search?manview[man_category]=\\2&manview[man_name]=\\1&manview[strict]=true\">\\1(\\2)</a>\\3 ")
       line.sub!(/^\.Sh\s+(.+)/, "<br><br><b>\\1</b><br>")
+      # order matters here!
+      line.sub!(/^\.Nm\s+:\s*$/, "<b>#{name}</b>:")
+      line.sub!(/^\.Nm\s+(.+)\s*(,)*/, " <b>\\1</b>\\2 ")
       line.sub!(/^\.Nm\s+(.+)/, "<b>\\1</b>")
       line.sub!(/^\.Nm\s*$/, "<b>#{name}</b>")
+
       line.sub!(/^\.Cm\s+(.+)/, "<b>\\1</b>")
+      line.sub!(/^\.Cd\s+(.+)/, "<b>\\1</b>")
       line.sub!(/^\.Fd\s+(.+)/, "<b>\\1</b><br>")
+      line.sub!(/^\.Ft\s+(.+)/, "<u>\\1</u><br>")
       line.sub!(/^\.Pq\s+Ar\s+([a-zA-Z0-9]+)/, " (<u>\\1</u>) ")
       line.sub!(/^\.Pa\s+(.+)/, "<u>\\1</u>")
+      line.sub!(/^\.Fa\s+(.+)/, "<u>\\1</u>")
+      line.sub!(/^\.Va\s+(.+)/, "<u>\\1</u>")
       line.sub!(/^\.Op\s+Fl\s+(.+)Ar\s+(.+)/, " [<b>-\\1</b><u>\\2</u>]")
       line.sub!(/^\.Op\s+Fl\s+(.+)/, " [<b>-\\1</b>]")
       line.sub!(/^\.Op\s+Ar\s+(.+)/, " [<u>\\1</u>]")
+      line.sub!(/^\.Fl\s+(.+)\s+Ar\s+(.+)\s+\|\s+Fl\s+(.+)\s+Ar\s+(.+)/, "<b>-\\1</b> \\2 | <b>-\\3</b> \\4")
+      line.sub!(/^\.Fl\s+(.+)\s+Ar\s+(.+)/, "<b>-\\1</b> \\2")
       line.sub!(/^\.Ar\s+([a-zA-Z0-9]+)/, " <u>\\1</u> ")
+      line.sub!(/^\.Dl\s+(.+)/, "<p><PRE>\\1</PRE></p>")
       line.sub!(/^\.Oo/, '[')
       line.sub!(/^\.Oc/, ']')
       line.sub!(/^\.Dq\s*([a-zA-Z0-9]+)\s*([a-zA-Z0-9]*)/, "\"\\1 \\2\"")
-      line.sub!(/^\.It\s+Fl\s+(.+)/, "<b>-\\1</b>")
-      line.sub!(/^\.It\s+Ar\s+(.+)/, "<u>\\1</u>")
+      line.sub!(/^\.Sq\s+(.+)\s+(.+)/, "'\\1'\\2")
+      line.sub!(/^\.Sq\s+(.+)/, "'\\1'")
+      line.sub!(/^\.Bd\s+(.+)/, "<p><PRE>")
+      line.sub!(/^\.Ed\s*/, "</PRE></p>")
 
+      if !line.sub!(/^\.In\s+(.+)/, "<b>#include &lt;\\1&gt;</b><br>").nil? &&
+         atext[index + 1].to_s !~ /^\.In\s+(.+)/
+        line += "<br>"
+      end
+
+      if line =~ /^\.Fn\s+.+\s+.+/
+        line.sub!(/^\.Fn\s+/, '')
+        items = line.gsub(/\s+/,' ').split(/&quot;/)
+        items = line.gsub(/\s+/,' ').split(/ /) if items.size == 1
+        items.each_index do |index|
+          item = items[index]
+          next if item =~ /^\s*$/
+          if index == 0
+            line = "<b>#{item}</b>("
+          elsif item == items.last
+            line += "<u>#{item}</u>);<br><br>"
+          else
+            line += "<u>#{item}</u>, "
+          end
+        end
+      elsif line =~ /^\.Fn\s+.+/
+        line.sub!(/^\.Fn\s+(.+)/, "<b>\\1()</b>")
+      end
+
+      if atext[index + 1].to_s =~ /^\.(El|It)/ && close_tr && columns.nil?
+        line += "</th></tr>"
+        close_tr = false
+      end
+
+      # Begin List
+      # column list
+      if line =~ /\.Bl\s+-column/ && columns.nil?
+         columns = atext[index + 1].sub(/\.It.+/, '').sub(/\s+/, ' ').split(/\s+/).size
+         line = "<table border=\"0\" style=\"text-align:left;\" cellpadding=\"10\">"
+      end
+
+      if !columns.nil? && line =~ /^\.It.+/
+         items = line.sub(/\.It\s+(Sy\s*)*/, '').sub(/\s+/, ' ').split(/\s+/)
+         line = "<tr>"
+         items.each do | item |
+           line += "<th>#{item}</th>"
+         end
+         line += "</tr>"
+      end
+
+      line.sub!(/^\.Bl(.+)/, "<table border=\"0\" style=\"text-align:left;\" cellpadding=\"10\">")
+
+      # List Item
+      close_tr = true if !line.sub!(/^\.It\s+Fl\s+(.+)\s+Ar\s+(.+)/, "<tr><th valign=\"top\"><b>-\\1</b> <u>\\2</u></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Fl\s+(.+)/, "<tr><th valign=\"top\"><b>-\\1</b></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Cm\s+(.+)\s+Ar\s+(.+)/, "<tr><th valign=\"top\"><b>\\1</b> <u>\\2</u></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Cm\s+(.+)/, "<tr><th valign=\"top\"><b>\\1</b></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Ar\s+(.+)/, "<tr><th valign=\"top\"><u>\\1</u></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Aq\s+Pa\s+(.+)/, "<tr><th valign=\"top\"><u>&lt;\\1&gt;</u></th><th>").nil? ||
+          !line.sub!(/^\.It\s+Bq\s+Er\s+(.+)/, "<tr><th valign=\"top\">[\\1]</th><th>").nil? ||
+          !line.sub!(/^\.It\s*/, "<tr><th valign=\"top\"></th><th>").nil?
+
+      # End List
+      if !line.sub!(/^\.El\s*/, "</table>").nil?
+        columns = nil
+      end
+
+      # some cleanup with spaces
       line.sub!(/\s+(\")+/, "\\1")
 
       line = ' ' + line
       line.sub!(/\s+/, ' ')
       line.sub!(/\s+\./, '.')
 
-      ntext.push(line) if !line.empty?
+      ntext.push(line + "\n") if !line.empty?
     end
 
     return ntext.to_s
