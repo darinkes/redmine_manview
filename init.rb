72f94c3f5454d32cb980bad9d8fd0814deca409c
Stefan Rinkes <stefan.rinkes@gmail.com>
2011-02-25 19:51:25 +0100

    add ManView-Link to Main-Page

--- init.rb
+++ init.rb
@@ -7,4 +7,7 @@ Redmine::Plugin.register :redmine_manpage do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  menu :application_menu, :manview, { :controller => 'manview', :action => 'index' }, :caption => 'ManView'
+
 end
